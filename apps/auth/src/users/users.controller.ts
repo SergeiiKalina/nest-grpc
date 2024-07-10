808227e5f2c6dcd3bee5a4fe2cfd350c4d6f51fe
Sergeii Kalyna <setserg02@gmail.com>
2024-07-10 11:19:16 +0300

    init

---
+++ apps/auth/src/users/users.controller.ts
@@ -0,0 +1,43 @@
+import { Controller } from '@nestjs/common';
+import { UsersService } from './users.service';
+import {
+  UserServiceController,
+  CreateUserDto,
+  UpdateUserDto,
+  UserServiceControllerMethods,
+  FindOneUserDto,
+  PaginationDto,
+  Users,
+} from '@app/common';
+import { Observable } from 'rxjs';
+
+@Controller()
+@UserServiceControllerMethods()
+export class UsersController implements UserServiceController {
+  constructor(private readonly usersService: UsersService) {}
+
+  createUser(createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  findAllUsers() {
+    return this.usersService.findAll();
+  }
+
+  findOneUsers(findOneUserDto: FindOneUserDto) {
+    return this.usersService.findOne(findOneUserDto.id);
+  }
+
+  updateUser(updateUserDto: UpdateUserDto) {
+    return this.usersService.update(updateUserDto.id, updateUserDto);
+  }
+
+  removeUser(findOneUserDto: FindOneUserDto) {
+    return this.usersService.remove(findOneUserDto.id);
+  }
+  queryUsers(
+    paginationDtoStream: Observable<PaginationDto>,
+  ): Observable<Users> {
+    return this.usersService.queryUsers(paginationDtoStream);
+  }
+}
